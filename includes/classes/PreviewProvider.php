e37ce63740d0ffb490ac46384276c1f8a52de946
marquelBennett04 <marquelnb@gmail.com>
2023-01-05 00:11:14 -0500

    Added in sample sql files for thumbanils, titles, and video files. Also added code that allows user to see previews.

---
+++ includes/classes/PreviewProvider.php
@@ -0,0 +1,47 @@
+<?php
+
+class PreviewProvider{
+
+private $con, $username;
+
+    public function __construct($con, $username){
+        $this->con = $con;
+        $this->username = $username;
+    }
+
+    public function createPreviewVideo($entity){
+        
+        if($entity == null){
+            $entity = $this->getRandomEntity();
+        }
+
+        $id = $entity->getId();
+        $name = $entity->getName();
+        $preview = $entity->getPreview();
+        $thumbnail = $entity->getThumbnail();
+
+        return "<div class = 'previewContainer'>
+
+                    <img src = '$thumbnail' class = 'previewImage' hidden>
+
+                    <video autoplay muted class = 'previewVideo'>
+                        <source src = '$preview' type = 'video/mp4'>
+                    </video>
+
+                </div>";
+
+        
+    }
+
+    private function getRandomEntity(){
+
+        $query = $this->con->prepare("SELECT * FROM entities ORDER BY RAND() LIMIT 1");
+        $query->execute();
+
+        $row = $query->fetch(PDO::FETCH_ASSOC);
+        
+        return new Entity($this->con, $row);
+    }
+}
+
+?>
--- index.php
+++ index.php
@@ -1,7 +1,14 @@
 <?php
 require_once("includes/config.php");
+require_once("includes/classes/PreviewProvider.php");
+require_once("includes/classes/Entity.php");
 
 if(!isset($_SESSION["userLoggedIn"])){
     header("Location: register.php");
 }
+
+$userLoggedIn = $_SESSION["userLoggedIn"];
+
+$preview = new PreviewProvider($con, $userLoggedIn);
+echo $preview->createPreviewVideo(null);
 ?>
