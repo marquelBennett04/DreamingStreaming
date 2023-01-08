83e5f3bf9d59bee48e4a099ee0e7a0c12273a7f6
marquelBennett04 <marquelnb@gmail.com>
2023-01-07 23:33:58 -0500

    Adding in entities and categories

---
+++ includes/classes/CategoryContainers.php
@@ -0,0 +1,54 @@
+<?php
+
+class CategoryContainers{
+
+private $con, $username;
+
+    public function __construct($con, $username){
+        $this->con = $con;
+        $this->username = $username;
+    }
+
+    public function showAllCategories(){
+        $query = $this->con->prepare("SELECT * FROM categories");
+        $query->execute();
+
+        $html = "<div class='previewCategories'>";
+
+        while($row = $query->fetch(PDO::FETCH_ASSOC)) {
+            $html .= $this->getCategoryHtml($row, null, true, true);
+        }
+
+        return $html . "</div>";
+    }
+
+    private function getCategoryHtml($sqlData, $title, $tvShows, $movies) { 
+        $categoryId = $sqlData["id"];
+        $title = $title == null ? $sqlData["name"] : $title;
+
+        if($tvShows && $movies){
+            $entities = EntityProvider::getEntities($this->con, $categoryId, 30);
+        }
+        else if($tvShows){
+
+        }
+        else{
+
+        }
+
+        if(sizeof($entities) == 0){
+            return;
+        }
+
+        $entitiesHtml = "";
+
+        foreach($entities as $entity){
+            $entitiesHtml .= $entity->getName();
+
+        }
+
+        return $entitiesHtml . "<br>"; 
+
+    }
+}
+?>
