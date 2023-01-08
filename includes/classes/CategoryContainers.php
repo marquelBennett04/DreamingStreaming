a54ea54cd547a3e32b59f85041dafdbdd9fb6438
marquelBennett04 <marquelnb@gmail.com>
2023-01-08 00:04:30 -0500

    Basic file changes

--- includes/classes/CategoryContainers.php
+++ includes/classes/CategoryContainers.php
@@ -1,16 +1,15 @@
 <?php
+class CategoryContainers {
 
-class CategoryContainers{
+    private $con, $username;
 
-private $con, $username;
-
-    public function __construct($con, $username){
+    public function __construct($con, $username) {
         $this->con = $con;
         $this->username = $username;
     }
 
-    public function showAllCategories(){
-        $query = $this->con->prepare("SELECT * FROM categories");
+    public function showAllCategories() {
+        $query = $this->con->prepare("SELECT * FROM `categories`");
         $query->execute();
 
         $html = "<div class='previewCategories'>";
@@ -22,33 +21,32 @@ private $con, $username;
         return $html . "</div>";
     }
 
-    private function getCategoryHtml($sqlData, $title, $tvShows, $movies) { 
+    private function getCategoryHtml($sqlData, $title, $tvShows, $movies) {
         $categoryId = $sqlData["id"];
         $title = $title == null ? $sqlData["name"] : $title;
 
-        if($tvShows && $movies){
+        if($tvShows && $movies) {
             $entities = EntityProvider::getEntities($this->con, $categoryId, 30);
         }
-        else if($tvShows){
-
+        else if($tvShows) {
+            // Get tv show entities
         }
-        else{
-
+        else {
+            // Get movie entities
         }
 
-        if(sizeof($entities) == 0){
+        if(sizeof($entities) == 0) {
             return;
         }
 
         $entitiesHtml = "";
 
-        foreach($entities as $entity){
+        foreach($entities as $entity) {
             $entitiesHtml .= $entity->getName();
-
         }
 
-        return $entitiesHtml . "<br>"; 
-
+        return $entitiesHtml . "<br>";
     }
+
 }
 ?>
