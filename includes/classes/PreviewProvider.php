0d8da133a120b2b922de71b06eff69a0254ba8b6
marquelBennett04 <marquelnb@gmail.com>
2023-01-08 00:44:30 -0500

    Added in changes to fix a pervious bug

--- includes/classes/PreviewProvider.php
+++ includes/classes/PreviewProvider.php
@@ -1,17 +1,16 @@
 <?php
+class PreviewProvider {
 
-class PreviewProvider{
+    private $con, $username;
 
-private $con, $username;
-
-    public function __construct($con, $username){
+    public function __construct($con, $username) {
         $this->con = $con;
         $this->username = $username;
     }
 
-    public function createPreviewVideo($entity){
+    public function createPreviewVideo($entity) {
         
-        if($entity == null){
+        if($entity == null) {
             $entity = $this->getRandomEntity();
         }
 
@@ -20,37 +19,43 @@ private $con, $username;
         $preview = $entity->getPreview();
         $thumbnail = $entity->getThumbnail();
 
-        //TODO ADD SUB
+        // TODO: ADD SUBTITLE
 
-        return "<div class = 'previewContainer'>
+        return "<div class='previewContainer'>
 
-                    <img src = '$thumbnail' class = 'previewImage' hidden>
+                    <img src='$thumbnail' class='previewImage' hidden>
 
-                    <video autoplay muted class = 'previewVideo' onended='previewEnded()'>
-                        <source src = '$preview' type = 'video/mp4'>
+                    <video autoplay muted class='previewVideo' onended='previewEnded()'>
+                        <source src='$preview' type='video/mp4'>
                     </video>
 
-                    <div class= 'previewOverlay'>
+                    <div class='previewOverlay'>
+                        
                         <div class='mainDetails'>
                             <h3>$name</h3>
 
-                            <div class= 'buttons'>
-                                <button><i class='fa-solid fa-play'></i> Play</button>
-                                <button onclick='volumeToggle(this)'><i class='fa-solid fa-volume-xmark'></i></button>
-                            </div
-                        </div>
-                    </div>
+                            <div class='buttons'>
+                                <button><i class='fas fa-play'></i> Play</button>
+                                <button onclick='volumeToggle(this)'><i class='fas fa-volume-mute'></i></button>
+                            </div>
 
+                        </div>
+
+                    </div>
+        
                 </div>";
 
+    }
+
+    private function getRandomEntity() {
+
+        $query = $this->con->prepare("SELECT * FROM entities ORDER BY RAND() LIMIT 1");
+        $query->execute();
+
+        $row = $query->fetch(PDO::FETCH_ASSOC);
         
+        return new Entity($this->con, $row);
     }
 
-    private function getRandomEntity(){
-
-       $entity = EntityProvider::getEntities($this->con, null, 1);
-       return $entity[0];
-    }
 }
-
 ?>
